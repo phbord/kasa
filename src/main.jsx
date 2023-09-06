129b6c4f32a1e092500246cd5294664af7b0b57f
phbord <phbord@gmail.com>
2023-09-06 14:27:51 +0200

    evo

--- src/main.jsx
+++ src/main.jsx
@@ -15,7 +15,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path='/kasa' element={<Layout />}>
+        <Route path='/' element={<Layout />}>
           {/* Pages */}
           <Route index 
                   element={<Suspense fallback={Loader}><Home /></Suspense>} />
